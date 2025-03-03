101c2c7f3a3dea200caea16a6fd275f20db6cc35
Charles Rhodes <crhodes76@users.noreply.github.com>
2025-03-03 07:43:48 -0500

    tweak

--- app.py
+++ app.py
@@ -4,14 +4,17 @@ import csv
 from data_model import DataModel, load_excel_files
 
 app = Flask(__name__)
+data_model = None
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    global data_model
     results = None
     if request.method == 'POST':
         excel_directory = request.form['directory']
         search_term = request.form['search_term']
-        data_model = load_excel_files(excel_directory)
+        if data_model is None:
+            data_model = load_excel_files(excel_directory)
         results = data_model.query_like(search_term)
     return render_template('index.html', results=results)
 
